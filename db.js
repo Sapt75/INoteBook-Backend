40d16f498e8376156e05f5db87123bd8d8dd976e
Sapt75 <109506771+Sapt75@users.noreply.github.com>
2023-02-20 17:24:11 +0530

    Final Commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 // const mongoUri = "mongodb://localhost:27017/iNoteBook"
-const mongoUri = "mongodb+srv://inotebook:abcd@1234@cluster0.daudrwc.mongodb.net/iNoteBook?retryWrites=true&w=majority"
+const mongoUri = "mongodb+srv://inotebook:inotebook@cluster0.daudrwc.mongodb.net/iNoteBook?retryWrites=true&w=majority"
 
 
 function connectMongo() {
